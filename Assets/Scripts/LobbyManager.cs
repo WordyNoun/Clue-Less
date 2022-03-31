29cbacf5dcb3ce4ce3444c979a5aab9f0f2516c8
Christian Yi <73957293+Chris-UMD@users.noreply.github.com>
2022-03-31 16:00:16 -0400

    Pick Name

---
+++ Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+using TMPro;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    public TMP_Text roomName;
+
+    public List<PlayerItem> playerItemsList = new List<PlayerItem>();
+    public PlayerItem playerItemPrefab;
+    public Transform playerItemParent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void OnJoinedRoom()
+    {
+        roomName.text = PhotonNetwork.CurrentRoom.Name;
+        UpdatePlayerList();
+    }
+
+    void UpdatePlayerList()
+    {
+        foreach (PlayerItem item in playerItemsList)
+        {
+            Destroy(item.gameObject);
+        }
+        playerItemsList.Clear();
+
+        if (PhotonNetwork.CurrentRoom == null)
+        {
+            return;
+        }
+
+        foreach (KeyValuePair<int, Player> player in PhotonNetwork.CurrentRoom.Players)
+        {
+            PlayerItem newPlayerItem = Instantiate(playerItemPrefab, playerItemParent);
+            newPlayerItem.SetPlayerInfo(player.Value);
+            playerItemsList.Add(newPlayerItem);
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        UpdatePlayerList();
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        UpdatePlayerList();
+    }
+}
