00728ac468832b9559ed483cc64a19a3ca8a55ce
Christian Yi <73957293+Chris-UMD@users.noreply.github.com>
2022-04-18 15:27:07 -0400

    Created GameBoard Class

--- src/GameBoardLocation.java
+++ src/GameBoardLocation.java
@@ -1,9 +1,5 @@
 /**
- * 
- */
-
-/**
- * @author katie
+ * GameBoardLocation defines a location where a player can reside and whether it is occupied if it is a hallway
  *
  */
 public class GameBoardLocation {
@@ -11,4 +7,30 @@ public class GameBoardLocation {
     public String GameBoardLocationType;
     public boolean hallwayOccupied;
     
+    public GameBoardLocation(String locName, String locType, boolean occupied) {
+    	GameBoardLocationName = locName;
+    	GameBoardLocationType = locType;
+    	hallwayOccupied = occupied;
+    }
+    
+    public String getLocationName() {
+    	return GameBoardLocationName;
+    }
+    
+    public String getLocationType() {
+    	return GameBoardLocationType;
+    }
+    
+    public boolean getOccupied() {
+    	return hallwayOccupied;
+    }
+    
+    public void changeOccupiedState(boolean occupied) {
+    	hallwayOccupied = occupied;
+    }
+    
+    @Override
+    public String toString() {
+    	return "Name: " + GameBoardLocationName + " Type: " + GameBoardLocationType + " Hallway Occupied: " + hallwayOccupied; 
+    }
 }
